99951afaf961b99830fdc7ce1b04cdbbac232e0d
Vincent Zhen <vincent@Vincents-MacBook-Air.local>
2025-09-29 00:21:32 -0400

    adjusted the size of the canvas

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import * as fabric from "fabric";
 function Home() {
   const canvasref = useRef(null)
   const [canvas, setCanvas] = useState(null)
+  // Load a small grid texture (e.g. 40x40 PNG or SVG)
 
   useEffect (() => {
     const fabriccanvas = new fabric.Canvas(canvasref.current, {
@@ -24,8 +25,7 @@ function Home() {
     fabriccanvas.freeDrawingBrush.color = "#000000"
     fabriccanvas.freeDrawingBrush.width = 5
     setCanvas(fabriccanvas)
-
-
+    
     return () => {
       fabriccanvas.dispose()
     }
