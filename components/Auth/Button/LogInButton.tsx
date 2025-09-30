29eb43c23e8dcb118e19887ef2eba9ef9d05920c
Carson Wang <carsonwang.001@gmail.com>
2025-09-30 01:42:12 -0400

    files for auth buttons

---
+++ components/Auth/Button/LogInButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type LoginButtonProps = {
+  
+};
+
+const LoginButton:React.FC<LoginButtonProps> = () => {
+  
+  return <div>Have a good coding</div>
+}
+export default LoginButton;
